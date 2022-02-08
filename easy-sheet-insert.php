aae419d6be519272096d5a5ca9ccd6a9203156b2
r1ck14 <67036277+r1ck14@users.noreply.github.com>
2022-02-07 20:48:14 -0500

    Update easy-sheet-insert.php
    Simplified a couple of sections. Removed unnecessary code.

--- easy-sheet-insert.php
+++ easy-sheet-insert.php
@@ -11,13 +11,11 @@ License: GPLv2 or later
 Text Domain: easy-sheet-insert
 */
 
-
 /*
 ######################
 # ADMIN MENU FOLLOWS #
 ######################
 */
-
 
 add_action( 'admin_menu', 'esi_admin_menu' );
 
@@ -42,19 +40,20 @@ function esi_admin_page( $atts ){
 	<div class="wrap">
 		<h2>Easy Sheet Insert</h2>
 		<h3>Uses for this plugin</h3>
-		<p>
-			This plugin can be used to easily allow for changing data on a page (such as prices) without even needing to enter the WordPress dashboard. One can just edit the data in a Google sheet and have the data instantly be reflected on their website.
-		</p>
+		<p>This plugin can be used to easily allow for changing data on a page (such as prices) without even needing to enter the WordPress dashboard. One can just edit the data in a Google sheet and have the data instantly be reflected on their website.</p>
 		<h3>How to use this plugin</h3>
-		<p>
-			This plugin adds a shortcode which allows the insertion of data from a cell in a Google Sheet to be displayed on a page. To use this, you will need 3 things: <ul>
+		<p>This plugin adds a shortcode which allows the insertion of data from a cell in a Google Sheet to be displayed on a page. To use this, you will need 3 things: 
+		<ul>
 		<li>•The ID of a Google Sheet that has sharing enabled (anyone with link can view)</li>
 		<li>•An API key from the Google account the sheet is in with the Sheets API enabled</li>
 		<li>•The ID of the cell where the data is</li>
 		</ul>
-		The shortcode needs one parameter to function: The cell you want to pull data from. It looks like this:<br><br>
-		<span style="font-weight: bold;">[esi_sheetpull cell="[CELL HERE]"]</span><br><br>
-		Simply replace the bracketed text (leave the quotes) with the appropriate data, eg. [esi_sheetpull cell="A1"].<br><br>
+		The shortcode needs one parameter to function: The cell you want to pull data from. It looks like this:
+		<br><br>
+		<span style="font-weight: bold;">[esi_sheetpull cell="[CELL HERE]"]</span>
+		<br><br>
+		Simply replace the bracketed text (leave the quotes) with the appropriate data, eg. [esi_sheetpull cell="A1"].
+		<br><br>
 		Cells are identified with  an uppercase letter and a number (eg "A1" or "C5"). Formulas can be placed in the cells in the spreadsheet and the plugin will pull the result.
 		</p><br>
 		<!--FORM FOR GETTING THE API-->
@@ -95,10 +94,8 @@ function esi_admin_page( $atts ){
 ######################################
 */
 
-
 //Pull data from google sheet and display
 function esi_pullSheetData( $attr ) {
-	$test = 'test';
 	$args = shortcode_atts( array(
 		'cell' => 'A1'
 	), $attr );
